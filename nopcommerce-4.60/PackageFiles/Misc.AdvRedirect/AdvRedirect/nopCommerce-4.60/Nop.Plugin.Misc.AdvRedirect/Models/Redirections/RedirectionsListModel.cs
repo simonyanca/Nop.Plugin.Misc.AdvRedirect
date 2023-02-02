8b340bfe7b69ab510d5fa1cc4e1b2eefbccd07c3
gerard.tous <gerard.tous@perfumesclub.com>
2023-02-02 14:59:17 +0100

    versionado

---
+++ nopcommerce-4.60/PackageFiles/Misc.AdvRedirect/AdvRedirect/nopCommerce-4.60/Nop.Plugin.Misc.AdvRedirect/Models/Redirections/RedirectionsListModel.cs
@@ -0,0 +1,12 @@
+﻿using Nop.Plugin.Misc.AdvRedirect.Models.Redirections;
+using Nop.Web.Framework.Models;
+
+namespace Nop.Plugin.Tax.Avalara.Models.Log
+{
+    /// <summary>
+    /// Represents a tax transaction log list model
+    /// </summary>
+    public record RedirectionsListModel : BasePagedListModel<RedirectionModel>    
+    {
+    }
+}
